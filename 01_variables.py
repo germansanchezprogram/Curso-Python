19cce6422ec71f45a873c72fe20f175572f145cf
German Program <germansanchezprogram@gmail.com>
2023-12-01 20:22:38 -0300

    sube

---
+++ 01_variables.py
@@ -0,0 +1,15 @@
+variable_string = "hola amigos"
+print(variable_string)
+
+variable_int = 4
+print(variable_int)
+print(type(variable_int))
+
+variable_boolean = True
+print(variable_boolean)
+
+my_int_to_str_variable = str(variable_int)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+print(variable_string, variable_int, variable_boolean)
